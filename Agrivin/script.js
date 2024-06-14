45ca0cc6e649c271dd9f107af5b7353283793e9c
yifangyam <it-yifang2@outlook.com>
2024-06-14 20:37:58 +0700

    Delete Agrivin directory

--- Agrivin/script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  "use strict";
-  $(".pCard_add").click(function () {
-    $(".pCard_card").toggleClass("pCard_on");
-    $(".pCard_add i").toggleClass("fa-plus fa-minus");
-  });
-});
